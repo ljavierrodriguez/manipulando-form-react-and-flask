f150e11b80ba3673fe0dee256df0d23220598979
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2022-05-23 13:31:11 +0000

    Upload Files

--- api/src/app.py
+++ api/src/app.py
@@ -24,7 +24,7 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_DEBUG'] = True
+app.config['MAIL_DEBUG'] = False
 app.config['MAIL_SUPPRESS_SEND'] = False
 
 
